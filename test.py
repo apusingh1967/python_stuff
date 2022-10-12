74ae0753fb3c2b824f9b571d0eb857b8b575b319
apurvasingh <apusingh1967@gmail.com>
2022-10-12 19:10:24 -0400

    one

---
+++ test.py
@@ -0,0 +1,27 @@
+g = {'a': ['b', 'c'],
+     'b': ['c', 'd'],
+     'c': ['e'],
+     'd': ['f'],
+     'e': ['f']
+     }
+
+
+def traverse(q):
+    from collections import deque
+    q = deque()
+    visited = {'a'}
+    q.appendleft('a')
+    while len(q) != 0:
+        print(q)
+        x = q.popleft()
+        print(x)
+        edges = g.get(x)
+        if edges is not None:
+            for e in edges:
+                if e not in visited:
+                    q.appendleft(e)
+                    visited.add(e)
+
+
+traverse(g)
+
